7c10633ef1197f218713d519228e1846f9a62538
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 13:52:51 +0300

    implemented task and tests

---
+++ src/arrayMethodSplice.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Implement method Splice
+ *
+ */
+
+function applyCustomSplice() {
+  [].__proto__.splice2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomSplice;
