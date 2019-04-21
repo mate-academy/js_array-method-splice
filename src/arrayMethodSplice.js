7a6ffaa84d121c2b129288700dce2ed27408e021
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-21 20:44:43 +0300

    all tests passed, but need improvements

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -8,54 +8,38 @@ function applyCustomSplice() {
     const initialLength = this.length;
     const argLength = items.length;
     let removedItems = [];
-    let temp = [];
+    let buffer = [];
 
+    if (!arguments.length) return removedItems;
     if (start < 0) start += initialLength;
-    if (start * -1 > initialLength) {
+    if ((start * -1) > initialLength) {
       removedItems = [...this];
       this.length = 0;
       return removedItems;
     }
     if (start === 0) return this;
+    if (start > initialLength) return removedItems;
 
     if (deleteCount > initialLength) deleteCount = 0;
-    if (deleteCount === 0 && !items) return removedItems;
+    if (deleteCount === 0 && (!items)) return removedItems;
 
-    for (let i = initialLength; i > start; i--) {
-      removedItems.push(this.pop());
-    }
+    removedItems = this.slice(start);
+    this.length = initialLength - (initialLength - start);
 
-    if (deleteCount && argLength <= 0) {
-      removeFromBuffer();
-      putTogether(this);
-    }
-    if (argLength > 0) {
-      removeFromBuffer();
-      insertArgs(this);
-      putTogether(this);
-    }
-
-    function removeFromBuffer() {
-      temp = removedItems.slice();
-      removedItems = [];
-      for (let i = 0; i < deleteCount; i++) {
-        removedItems.push(temp.pop());
-      }
-      removedItems.reverse();
-    }
-
-    function insertArgs(obj) {
-      items.forEach(item => obj.push(item));
-    }
-
-    function putTogether(obj) {
-      let tempLength = temp.length;
-      for (let j = 0; j < tempLength; j++) {
-        obj.push(temp.pop());
+    if (deleteCount || deleteCount === 0) {
+      buffer = removedItems.slice(deleteCount);
+      removedItems = removedItems.slice(0, deleteCount);
+      if (argLength > 0) {
+        buffer = [...items, ...buffer];
       }
     }
-    return removedItems.reverse();
+    buffer.forEach(item => this.push(item));
+    return removedItems;
   };
 }
 
+applyCustomSplice();
+let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+a.splice2();
+// [0,1,2,3,4,5,6].splice2(2)
 module.exports = applyCustomSplice;
