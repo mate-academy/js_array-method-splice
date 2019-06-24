ee274680808097a3076c00ffc4e58ad643917cd0
RavenVol <r.volianiuk@gmail.com>
2019-06-24 09:20:23 +0300

    Splice2 - Partial Solved by Raven

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,53 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
+  [].__proto__.splice2 = function(start = 0, deleteCount = this.length, ...items) {
     // write code here
+    if (!items) {
+      items = [];
+    }
+
+    const deletedItems = [];
+    const currLength = this.length;
+
+    let deleteStartIndex = start;
+    if (deleteStartIndex > currLength) {
+      deleteStartIndex = currLength;
+    } else if (deleteStartIndex < 0) {
+      deleteStartIndex = currLength + start;
+    }
+    if (deleteStartIndex < 0) {
+      deleteStartIndex = 0;
+    }
+
+    let deleteEndIndex = deleteStartIndex + deleteCount - 1;
+    if (deleteEndIndex > currLength - 1) {
+      deleteEndIndex = currLength - 1;
+    }
+
+    const lengthCorrection = items.length - (deleteEndIndex - deleteStartIndex + 1);
+    const newLength = currLength + lengthCorrection;
+
+    for (let i = 0; i < (deleteEndIndex - deleteStartIndex + 1); i++) {
+      deletedItems[i] = this[deleteStartIndex + i];
+    }
+
+    if (lengthCorrection > 0) {
+      for (let i = currLength - 1; i > deleteEndIndex; i--) {
+        this[i + lengthCorrection] = this[i];
+      }
+    } else if (lengthCorrection < 0) {
+      for (let i = deleteEndIndex + 1; i < currLength; i++) {
+        this[i + lengthCorrection] = this[i];
+      }
+    }
+
+    for (let i = 0; i < items.length; i++) {
+      this[deleteStartIndex + i] = items[i];
+    }
+
+    this.length = newLength;
+    return deletedItems;
   };
 }
 
