ced876c9827a8124d894cfc305a07e87d761abeb
bratyslav <beginner.altavista@gmail.com>
2019-06-24 10:06:22 +0300

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -10,22 +10,26 @@ function applyCustomSplice() {
     ...items
   ) {
     const returnArray = [];
+
     // проверка старта
     if (start === 0 || (start < 0 && Math.abs(start) > this.length)) {
       for (let i = 0; i < this.length; i++) {
         returnArray[returnArray.length] = this[i];
       }
+
       this.length = 0;
       return returnArray;
     } else if (!start) {
       return [];
     }
+
     let begin = start;
     if (begin > this.length) {
       begin = this.length;
     } else if (begin < 0) {
       begin = this.length + begin;
     }
+
     // проверка счетчика удаления
     let deleteAmount = deleteCount;
     if (deleteAmount > this.length - begin) {
@@ -33,30 +37,36 @@ function applyCustomSplice() {
     } else if (deleteAmount < 0) {
       deleteAmount = 0;
     }
+
     // записываем до старта
     const resultArray = [];
     for (let i = 0; i < begin; i++) {
       resultArray[resultArray.length] = this[i];
     }
+
     // записываем удаляемые значения в возвращаемый массив
     for (let i = begin; i < begin + deleteAmount; i++) {
       returnArray[returnArray.length] = this[i];
     }
+
     // вставляем новые значения
     if (items.length !== 0) {
       for (let i = 0; i < items.length; i++) {
         resultArray[resultArray.length] = items[i];
       }
     }
+
     // записываем оставшиеся значения
     for (let i = begin + deleteAmount; i < this.length; i++) {
       resultArray[resultArray.length] = this[i];
     }
+
     // переписываем получившийся массив в this
     this.length = 0;
     for (let i = 0; i < resultArray.length; i++) {
       this[this.length] = resultArray[i];
     }
+
     // возвращаем массив с удаленными значениями
     return returnArray;
   };
