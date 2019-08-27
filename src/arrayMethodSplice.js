9bcb7e7b7f3ff27b544e3311f23f3a98125e1afb
Rafler <amuronyk@gmail.com>
2019-08-27 14:46:43 +0300

    spacing

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -9,17 +9,22 @@ function applyCustomSplice() {
     const leftArr = [];
     const rightArr = [];
     const removed = [];
+
     if (arguments.length < 1 || start > this.length || deleteCount < 0) {
       return [];
     }
+
     let first = 0;
+
     start < 0 && start > -this.length
       ? (first = this.length + start)
       : start < -this.length || start === undefined
         ? (first = 0)
         : (first = start);
+
     const startPoint = first;
     let pauseFirst = 0;
+
     deleteCount !== undefined
       ? (pauseFirst = deleteCount + first)
       : (pauseFirst = this.length);
@@ -48,7 +53,9 @@ function applyCustomSplice() {
     for (let i = 0; i < splicedArr.length; i++) {
       this[i] = splicedArr[i];
     }
+
     this.length = splicedArr.length;
+
     return removed;
   };
 }
