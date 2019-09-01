f25bbdf8a674c49a4e7b1638fa84ff694290e0fa
yevhen-sahach <antitop04@gmail.com>
2019-09-01 11:57:08 +0300

    first commit

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,46 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(start, del, ...items) {
+    const beforeStartIndex = [];
+    const afterDeleteIndex = [];
+    const output = [];
+    const startIndex = start === undefined
+      ? 0
+      : start >= 0
+        ? start
+        : Math.max(start + this.length, 0);
+    const deleteCount = del === undefined
+      ? this.length - startIndex
+      : del;
+
+    if (deleteCount < 0
+      || start > this.length
+      || arguments.length === 0) {
+      return [];
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+      beforeStartIndex.push(this[i]);
+    }
+
+    for (let j = 0; j < (this.length - startIndex - deleteCount); j++) {
+      afterDeleteIndex.push(this[j + startIndex + deleteCount]);
+    }
+
+    for (let m = 0; m < deleteCount; m++) {
+      output.push(this[startIndex + m]);
+    }
+
+    const result = [...beforeStartIndex, ...items, ...afterDeleteIndex];
+
+    for (let k = 0; k < result.length; k++) {
+      this[k] = result[k];
+    }
+
+    this.length = result.length;
+
+    return output;
   };
 }
 
