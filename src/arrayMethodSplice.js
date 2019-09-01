f46c30016cdf6ed395336352733d4353f523b5fa
igaryok <igor.sobko@gmail.com>
2019-09-01 21:19:58 +0300

    changed according to reviwe

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,16 +5,30 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(
-    start = 0, deleteCount = this.length, ...items) {
+    start = 0,
+    deleteCount = this.length,
+    ...items
+  ) {
     const resultSplice = [];
     const partAfterDelete = [];
     let deleteFrom = start;
     let deleteNumber = deleteCount;
-    if (start < 0) { deleteFrom = start + this.length; }
-    if (start > this.length - 1) { deleteFrom = 0; deleteNumber = 0; }
-    if (start < 1 - this.length) { deleteFrom = 0; }
-    if (arguments.length === 0) { deleteFrom = this.length; }
-    if (deleteCount < 0) { deleteNumber = 0; }
+
+    if (start < 0) {
+      deleteFrom = start + this.length;
+    }
+    if (start > this.length - 1) {
+      deleteFrom = 0; deleteNumber = 0;
+    }
+    if (start < -this.length) {
+      deleteFrom = 0;
+    }
+    if (arguments.length === 0) {
+      deleteFrom = this.length;
+    }
+    if (deleteCount < 0) {
+      deleteNumber = 0;
+    }
 
     for (let i = 0; i < this.length; i++) {
       if (i >= deleteFrom && i < deleteFrom + deleteNumber) {
@@ -23,6 +37,7 @@ function applyCustomSplice() {
         partAfterDelete.push(this[i]);
       }
     }
+
     this.length = deleteFrom;
     this.push(...items, ...partAfterDelete);
 
