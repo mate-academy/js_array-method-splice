642cec4f90ca10c50ac6e2e1258a16a659bb453d
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-02 17:55:34 +0300

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,7 +5,28 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+    if (arguments.length === 0 || deleteCount < 0) {
+      return [];
+    } else if (start === undefined) {
+      start = 0;
+    }
+
+    const copyArr = [...this];
+    const deleteItems = [];
+    this.length = 0;
+
+    if (deleteCount === undefined && items.length === 0) {
+      deleteItems.push(...copyArr.slice(start));
+      this.push(...copyArr.slice(0, start));
+      return deleteItems;
+    }
+
+    this.push(...copyArr.slice(0, start));
+    this.push(...items);
+    this.push(...copyArr.slice(start + deleteCount));
+    deleteItems.push(...copyArr.slice(start, start + deleteCount));
+
+    return deleteItems;
   };
 }
 
