7d0dd2f216fbdaad25c0889aafb3720e62a3980b
Olexander <kravchenko.sania@gmail.com>
2020-01-29 20:14:09 +0200

    attempt 3

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -6,9 +6,9 @@
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
     const result = [];
-    let startIndex;
-    let deleteActualy;
-    const insertCount = items.length ? items.length : 0;
+    let startIndex = start;
+    let deleteActualy = deleteCount;
+    const insertCount = items.length || 0;
 
     if (!arguments.length || arguments[0] > this.length || deleteCount < 0) {
       return result;
@@ -20,20 +20,16 @@ function applyCustomSplice() {
       startIndex = this.length + start > 0 ? this.length + start : 0;
     } else if (!start) {
       startIndex = 0;
-    } else {
-      startIndex = start;
     }
 
     if (arguments.length === 1 || deleteCount > this.length - startIndex) {
       deleteActualy = this.length - startIndex;
     } else if (arguments.length > 1 && !deleteCount) {
       deleteActualy = 0;
-    } else {
-      deleteActualy = deleteCount;
     }
 
     if (deleteActualy > 0) {
-      for (let i = startIndex; i < startIndex + deleteActualy; ++i) {
+      for (let i = startIndex; i < startIndex + deleteActualy; i += 1) {
         result[i - startIndex] = this[i];
       }
     }
@@ -41,7 +37,7 @@ function applyCustomSplice() {
     const offset = deleteActualy - insertCount;
 
     if (offset > 0) {
-      for (let i = startIndex + deleteActualy; i < this.length; ++i) {
+      for (let i = startIndex + deleteActualy; i < this.length; i += 1) {
         this[i - offset] = this[i];
       }
       this.length = this.length - offset;
@@ -50,13 +46,13 @@ function applyCustomSplice() {
     if (offset < 0) {
       for (let i = this.length - offset - 1;
         i >= startIndex + insertCount;
-        --i
+        i -= 1
       ) {
         this[i] = this[i + offset];
       }
     }
 
-    for (let i = 0; i < insertCount; ++i) {
+    for (let i = 0; i < insertCount; i += 1) {
       this[startIndex + i] = items[i];
     }
 
