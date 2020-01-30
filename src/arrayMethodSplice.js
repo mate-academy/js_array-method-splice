74c3e9a56dfa2c5756442ca0ba0d03b7ecaaa62b
Vitalii <vitaliybondarenko1982@gmail.com>
2020-01-30 13:06:54 +0200

    rewrite

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -7,126 +7,54 @@
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
     const result = [];
+    const deletedItems = [];
 
-    let resultIndex = 0;
-
-    if (-start > this.length) {
-      for (let i = 0; i < this.length; i++) {
-        result[resultIndex] = this[i];
-        resultIndex++;
-      }
-      this.length = 0;
+    if (arguments.length === 0
+      || (deleteCount < 0)
+      || ((start >= this.length) && start !== 0)) {
+      return deletedItems;
     }
 
-    if (arguments.length === 1 && start > 0 && start <= this.length) {
-      for (let i = start; i < this.length; i++) {
-        result[resultIndex] = this[i];
-        resultIndex++;
-      }
+    let startSearch = start;
 
-      this.length = start;
+    if ((startSearch === undefined) || -startSearch > this.length) {
+      startSearch = 0;
+    } else if (startSearch < 0) {
+      startSearch = this.length + start;
     }
 
-    if (arguments.length === 1 && start < 0 && -start <= this.length) {
-      for (let i = this.length + start; i < this.length; i++) {
-        result[resultIndex] = this[i];
-        resultIndex++;
+    if (deleteCount === undefined) {
+      for (let i = startSearch; i < this.length; i++) {
+        deletedItems[deletedItems.length] = this[i];
       }
+      this.length = startSearch;
 
-      this.length = this.length + start;
+      return deletedItems;
     }
 
-    if (arguments.length === 2 && start === undefined && deleteCount > 0) {
-      const startIndex = 0;
-
-      for (let i = 0; i < deleteCount; i++) {
-        result[resultIndex] = this[startIndex + i];
-        resultIndex++;
-      }
-
-      for (let i = startIndex + deleteCount; i < this.length; i++) {
-        this[startIndex] = this[i];
-        this.length = this.length - deleteCount;
-      }
+    for (let i = startSearch; i < startSearch + deleteCount; i++) {
+      deletedItems[deletedItems.length] = this[i];
     }
 
-    if (arguments.length === 2 && start > 0 && deleteCount > 0) {
-      for (let i = 0; i < deleteCount; i++) {
-        result[resultIndex] = this[start + i];
-        resultIndex++;
-      }
-
-      for (let i = start + deleteCount; i < this.length; i++) {
-        this[start] = this[i];
-        this.length = this.length - deleteCount;
-      }
+    for (let i = 0; i < startSearch; i++) {
+      result[result.length] = this[i];
     }
 
-    if (arguments.length === 2 && start < 0 && deleteCount > 0) {
-      for (let i = 0; i < deleteCount; i++) {
-        result[resultIndex] = this[this.length + start + i];
-        resultIndex++;
-      }
-
-      for (let i = this.length + start + deleteCount; i < this.length; i++) {
-        this[this.length + start] = this[i];
-        this.length = this.length - deleteCount;
-      }
+    for (let i = 0; i < items.length; i++) {
+      result[result.length] = items[i];
     }
 
-    if (arguments.length > 2 && this.length === 0) {
-      for (let i = 0; i < items.length; i++) {
-        this[i] = items[i];
-      }
+    for (let i = startSearch + deleteCount; i < this.length; i++) {
+      result[result.length] = this[i];
     }
 
-    const diffDeleteAndAdded = items.length - deleteCount;
-    const thisLength = this.length;
-
-    if (start > 0 && deleteCount > 0 && items[0] !== undefined) {
-      for (let i = diffDeleteAndAdded; i > 0; i--) {
-        this[thisLength - 1 + i] = this[thisLength - i];
-      }
-
-      for (let i = 0; i < deleteCount; i++) {
-        result[resultIndex] = this[start + i];
-        resultIndex++;
-      }
-
-      for (let i = 0; i < items.length; i++) {
-        this[start + i] = items[i];
-      }
+    for (let i = 0; i < result.length; i++) {
+      this[i] = result[i];
     }
 
-    if (arguments.length > 2 && start > 0 && deleteCount === 0) {
-      for (let i = 0; i < items.length; i++) {
-        this[thisLength + i] = this[start + i];
-      }
+    this.length = result.length;
 
-      for (let i = 0; i < items.length; i++) {
-        this[start + i] = items[i];
-      }
-    }
-
-    if (items[0] === undefined && items.length === 1) {
-      for (let i = 0; i < deleteCount; i++) {
-        result[resultIndex] = this[start + i];
-        resultIndex++;
-      }
-
-      for (let i = 0; i < this.length - start - deleteCount; i++) {
-        this[start + 1 + i] = this[start + deleteCount + i];
-      }
-
-      this[start] = undefined;
-      this.length = this.length - deleteCount + 1;
-    }
-
-    if (arguments.length < 1 || start > this.length || deleteCount === 0) {
-      return [];
-    }
-
-    return result;
+    return deletedItems;
   };
 }
 
