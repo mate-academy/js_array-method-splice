1d93a51ddc6c70136fa79efb786af199ee433689
Vitalii Korol <vitaliikorol@192-168-0-102.rdsnet.ro>
2020-05-16 12:16:01 +0300

    solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,7 +5,62 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+    const deletedItems = [];
+    let startPoint = start;
+
+    if (!arguments.length) {
+      return deletedItems;
+    }
+
+    if (start > this.length) {
+      startPoint = this.length;
+    }
+
+    if (start < 0) {
+      startPoint = this.length + start;
+    }
+
+    if (startPoint < 0 || startPoint === undefined) {
+      startPoint = 0;
+    }
+
+    let deleteIndex = startPoint;
+    let deleteAmount = deleteCount;
+
+    if (
+      deleteAmount === undefined
+      || deleteAmount >= this.length - startPoint
+    ) {
+      deleteAmount = this.length - startPoint;
+    }
+
+    if (deleteAmount <= 0) {
+      deleteAmount = 0;
+    }
+
+    while (deleteAmount > 0) {
+      deletedItems.push(this[deleteIndex]);
+      deleteIndex++;
+      deleteAmount--;
+    }
+
+    for (let i = startPoint; i < this.length; i++) {
+      this[i] = this[i + deletedItems.length];
+    }
+
+    this.length = this.length - deletedItems.length;
+
+    this.length = this.length + items.length;
+
+    for (let i = this.length - 1; i > startPoint; i--) {
+      this[i] = this[i - items.length];
+    }
+
+    for (let i = startPoint; i < startPoint + items.length; i++) {
+      this[i] = items[i - startPoint];
+    }
+
+    return deletedItems;
   };
 }
 
