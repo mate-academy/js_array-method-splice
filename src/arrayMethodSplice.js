990d6980da869053cc2751a737ff3350c50ba8b2
Vovasef <vovasef@gmail.com>
2020-05-29 01:58:36 +0300

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,14 +5,12 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    const length = this.length;
-    let modified = [];
+    const initialLength = this.length;
     let startIndex = start;
-    let deleteCountIndex = deleteCount;
-    let modifiedIndex = 0;
+    let deleted = [];
 
     if (arguments.length === 0) {
-      return modified;
+      return deleted;
     }
 
     if (startIndex === undefined) {
@@ -20,62 +18,57 @@ function applyCustomSplice() {
     }
 
     if (startIndex < 0) {
-      if ((startIndex * -1) > length) {
+      if ((startIndex * -1) > initialLength) {
         startIndex = 0;
       } else {
-        startIndex = length + startIndex;
+        startIndex = initialLength + startIndex;
       }
-    } else if (startIndex > length) {
-      startIndex = length;
+    } else if (startIndex > initialLength) {
+      startIndex = initialLength;
     }
 
-    if (!deleteCountIndex && deleteCountIndex !== 0) {
-      for (let i = startIndex; i < length; i++) {
-        modified[modifiedIndex] = this[i];
-        modifiedIndex++;
+    if (!deleteCount && deleteCount !== 0) {
+      for (let i = startIndex; i < initialLength; i++) {
+        deleted[i - startIndex] = this[i];
       }
       this.length = startIndex;
 
-      return modified;
+      return deleted;
     }
 
     if (items.length === 0) {
-      if (deleteCountIndex > length - startIndex) {
+      if (deleteCount > initialLength - startIndex) {
         return this.splice2(startIndex);
-      } else if (deleteCountIndex < 1) {
-        return modified;
+      } else if (deleteCount < 1) {
+        return deleted;
       }
 
       const tail = this.splice2(startIndex);
 
-      for (let i = 0; i < deleteCountIndex; i++) {
-        modified[i] = tail[i];
+      for (let i = 0; i < deleteCount; i++) {
+        deleted[i] = tail[i];
       }
 
-      for (let i = deleteCountIndex; i < tail.length; i++) {
-        this[startIndex] = tail[deleteCountIndex];
-        startIndex++;
-        deleteCountIndex++;
+      for (let i = deleteCount; i < tail.length; i++) {
+        this[startIndex - deleteCount + i] = tail[i];
       }
 
-      return modified;
+      return deleted;
     }
 
-    const tailItems = this.splice2(startIndex + deleteCountIndex);
+    const tailItems = this.splice2(startIndex + deleteCount);
 
-    modified = this.splice2(startIndex);
+    deleted = this.splice2(startIndex);
 
     for (let i = 0; i < items.length; i++) {
-      this[startIndex] = items[i];
-      startIndex++;
+      this[startIndex + i] = items[i];
     }
 
     for (let i = 0; i < tailItems.length; i++) {
-      this[startIndex] = tailItems[i];
-      startIndex++;
+      this[startIndex + items.length + i] = tailItems[i];
     }
 
-    return modified;
+    return deleted;
   };
 }
 
