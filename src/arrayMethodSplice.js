101b5035679f48dd894866c8dc0021740fa7e172
Liliia <chuiko1112@gmail.com>
2020-05-28 17:20:10 +0300

    sol

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,45 +4,93 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount = 0, ...items) {
-    let newDeleteCounter = arguments[1];
-    let reassignedStart = arguments[0];
-    const removedList = [];
+  [].__proto__.splice2 = function(start, deleteCount, ...items) {
+    let newCounter = arguments[1];
+    let newStart = arguments[0];
+    let removedList = [];
+    const array = [...this];
 
-    if (start < 0) {
-      reassignedStart = this.length + start;
-    } else if (reassignedStart > this.length) {
-      reassignedStart = this.length - 1;
+    if (newCounter < 0) {
+      newCounter = 0;
     }
 
-    if (reassignedStart * (-1) > this.length) {
-      reassignedStart = 0;
+    if (Math.abs(arguments[0]) > this.length
+    && arguments[1] === undefined
+    && items.length === 0) {
+      if (arguments[0] > 0) {
+        removedList = [];
+      } else {
+        removedList = [...array];
+        this.length = 0;
+      }
+
+      return removedList;
     }
 
-    if (deleteCount > this.length) {
-      return [];
-    } else if (deleteCount < 0) {
-      newDeleteCounter = 0;
+    if (newCounter > (this.length - newStart)) {
+      newCounter = this.length - newStart;
     }
+    // example splice (1,2)
 
-    if (reassignedStart !== undefined
-      && newDeleteCounter === undefined
+    if (newStart !== undefined
+      && newCounter !== undefined
       && items.length === 0) {
-      newDeleteCounter = 1;
-      reassignedStart = 0;
-    }
+      for (let i = newStart; i < (newStart + deleteCount); i++) {
+        removedList[i - newStart] = this[i];
+      }
 
-    if (reassignedStart !== undefined
-      && newDeleteCounter !== undefined
-      && items.length === 0) {
-      for (let i = reassignedStart; i < this.length; i++) {
-        if (items.length === 0 && newDeleteCounter !== 0) {
-          i++;
-          removedList.push(this[i]);
+      let changed = false;
+
+      for (let i = 0; i < this.length; i++) {
+        if (i < newStart) {
+          this[i] = this[i];
+        } else {
+          if (changed === false) {
+            i += newCounter;
+            changed = true;
+            this[i - newCounter] = this[i];
+          } else {
+            this[i - newCounter] = this[i];
+          }
         }
       }
-      this.length = newDeleteCounter;
+      this.length = this.length - newCounter;
     }
+    // example for splice2(1, 2, 'a', 'b', 'c')
+
+    if (newStart !== undefined
+      && newCounter !== undefined
+      && items.length !== 0) {
+      for (let i = newStart; i < (newStart + deleteCount); i++) {
+        removedList[i - newStart] = this[i];
+      }
+
+      for (let i = 0; i < (array.length - newCounter + items.length); i++) {
+        if (i >= newStart) {
+          if (items[i - newStart] !== undefined) {
+            this[i] = items[i - newStart];
+          } else {
+            if ((array.length - newCounter - newStart) > 0) {
+              this[i] = array[i - newStart - items.length + newCounter + 1];
+            }
+          }
+        }
+      }
+      this.length = array.length - newCounter + items.length;
+    }
+
+    if (newStart !== undefined
+      && newCounter === undefined
+      && items.length === 0) {
+      newCounter = arguments[0];
+      newStart = 0;
+
+      for (let i = newCounter; i < this.length; i++) {
+        removedList[i - newCounter] = this[i];
+      }
+      this.length = newCounter;
+    }
+    console.log (newStart, newCounter, this.length, removedList, this, this.length, array);
 
     return removedList;
   };
