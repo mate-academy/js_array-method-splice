b0404de767d94c89ddd3e73c0ece457ddec6e548
Glib Rulev <velzepooz@gmail.com>
2020-05-31 12:03:55 +0300

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,53 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(
+    start = 0, deleteCount = this.length, ...items) {
+    if (arguments.length === 0) {
+      return [];
+    }
+
+    const spliced = [];
+    let startIndex = start;
+
+    if (startIndex < 0) {
+      startIndex += this.length;
+
+      if (startIndex < 0) {
+        startIndex = 0;
+      }
+    } else if (startIndex > this.length) {
+      startIndex = this.length;
+    }
+
+    const endIndex = deleteCount === this.length
+      ? this.length
+      : deleteCount + startIndex;
+
+    for (let i = startIndex; i < endIndex; i++) {
+      spliced.push(this[i]);
+
+      if (endIndex < this.length) {
+        this[i] = this[i + deleteCount];
+      }
+    }
+
+    this.length -= spliced.length;
+
+    if (items.length > 0) {
+      let itemsIndex = 0;
+
+      this.length += items.length;
+
+      for (let i = startIndex; itemsIndex < items.length; i++) {
+        if (i + items.length < this.length) {
+          this[i + items.length] = this[i];
+        }
+        this[i] = items[itemsIndex++];
+      }
+    }
+
+    return spliced;
   };
 }
 
