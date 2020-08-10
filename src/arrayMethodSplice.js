87473227f75f2ff5b86aef23da33d1cec190322d
artsashko <artyomsashko@gmail.com>
2020-08-10 14:03:05 +0300

    solution without 1 test

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,57 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(start,
+    deleteCount, ...items) {
+    let delCount = deleteCount || this.length - start;
+    let startIndex = start || 0;
+    let lastDeletedIndex = startIndex + delCount;
+    const added = [...items];
+    let deleted = [];
+    const afterDeleted = [];
+
+    if (startIndex > this.length
+      || (start === undefined && deleteCount === undefined)
+      || delCount < 0) {
+      return [];
+    }
+
+    if (Math.abs(startIndex) > this.length) {
+      deleted = [...this];
+      this.length = 0;
+
+      return deleted;
+    }
+
+    if (startIndex < 0) {
+      if (delCount === this.length - start) {
+        delCount = Math.abs(start);
+      }
+      startIndex += this.length;
+      lastDeletedIndex = startIndex + delCount;
+    }
+
+    for (let i = startIndex, j = 0; j < delCount; i++, j++) {
+      deleted[j] = this[i];
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+      added.unshift(this[i]);
+    }
+
+    if (deleteCount === 0) {
+      lastDeletedIndex = startIndex;
+      deleted.length = 0;
+    }
+
+    for (let i = lastDeletedIndex; i < this.length; i++) {
+      afterDeleted.push(this[i]);
+    }
+
+    this.length = 0;
+    this.push(...added, ...afterDeleted);
+
+    return deleted;
   };
 }
 
