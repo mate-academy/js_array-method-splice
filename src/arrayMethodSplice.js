558fc1ad327f5a6e86578a421dbf3bbcb644a307
olena <melolena33@gmail.com>
2021-03-09 15:11:14 +0200

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,7 +5,57 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+    const deleted = [];
+    const length = this.length;
+
+    let startPos = Number(start);
+
+    if (isNaN(startPos)) {
+      startPos = 0;
+    }
+
+    if (start < 0) {
+      startPos = start + length;
+    }
+
+    startPos = Math.max(0, startPos);
+    startPos = Math.min(startPos, length);
+
+    let deleteAmount = Number(deleteCount);
+
+    if (isNaN(deleteAmount)) {
+      deleteAmount = start ? length : 0;
+    }
+
+    if (deleteCount < 0) {
+      deleteAmount = 0;
+    } else {
+      deleteAmount = Math.min(deleteAmount, length - startPos);
+    }
+
+    if (items) {
+      const itemsLength = items.length;
+
+      for (let i = startPos; i < startPos + deleteAmount; i++) {
+        deleted[i - startPos] = this[i];
+      }
+
+      for (let i = startPos + deleteAmount, j = startPos + itemsLength;
+        i < length;
+        i++, j++) {
+        this[j] = this[i];
+      }
+
+      for (let i = 0; i < itemsLength; i++) {
+        this[startPos + i] = items[i];
+      }
+
+      this.length = length + itemsLength;
+    }
+
+    this.length -= deleteAmount;
+
+    return deleted;
   };
 }
 
