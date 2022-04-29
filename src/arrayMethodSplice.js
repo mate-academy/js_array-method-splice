beebb65f33bba7c25464f6ab369fe2c50e27a5a1
Roman Pavlenko <proman9973@gmail.com>
2022-04-29 20:19:20 +0300

    added comments

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -12,16 +12,19 @@ function applyCustomSplice() {
     const deleter = deleteCount;
     let count = 0;
 
+    //  блок для установки старту відліку
     if ((start === undefined) || (starter < -10)) {
       starter = 0;
     } else if (starter < 0) {
       starter += this.length;
     }
+    //  при порожньому визову функції видаємо порожній масив
 
     if (arguments.length === 0) {
       return [];
     }
 
+    // блок видалення із this та додача до result
     if (starter === 0) {
       for (let i = starter; i < deleter; i++) {
         result[count] = this[i];
@@ -42,6 +45,7 @@ function applyCustomSplice() {
     count = 0;
 
     let undefCount = 0;
+    //  переносимо undefined до кінця this
 
     for (let i = 0; i <= len; i++) {
       if (this[i] !== undefined) {
@@ -51,11 +55,13 @@ function applyCustomSplice() {
         undefCount++;
       }
     }
+    //  зменшуємо довжину this на кількість undefined
     this.length -= undefCount;
 
     count = 0;
     undefCount = 0;
 
+    //   переносимо undefined до кінця return
     for (let i = 0; i < result.length; i++) {
       if (result[i] !== undefined) {
         result[count] = result[i];
@@ -64,13 +70,19 @@ function applyCustomSplice() {
         undefCount++;
       }
     }
+    //  зменшуємо довжину result на кількість undefined
     result.length -= undefCount;
 
+    // довжина аргументів без start ta end
     const argsLen = arguments.length - 2;
 
     if (arguments.length > 2) {
       count = 2;
 
+      /*
+      Переносимо значення this[i] на позицію this[i + argsLen].
+      Та переписуємо значення на передані аргументи функції
+      */
       if (starter === 0) {
         for (let i = starter; i < argsLen; i++) {
           if (this[i] !== undefined) {
