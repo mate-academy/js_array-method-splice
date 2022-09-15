5655497b957e329bc44ba039ec6eb6da43e7ebd9
Max Bovtun <maxbovtun@gmail.com>
2022-09-15 22:59:33 +0300

    slice added

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -13,11 +13,46 @@
  * I did my personal .concat2() to be clear with it :)
  * |
  * Also, I tried to wrote a documentation for each step
- * to make sure my code is clear and understandable.
+ * inside of splice to make sure my code is clear and understandable.
+ * |
+ * =>== **EDITED**:
+ * =>== custom .slice method added 'cuz @Denis said
+ * =>== we couldn"t use regular methods of array ...damn))
  * |
  * <=== Have fun ===>
  */
+// additional <normalize> function for the SLICE method:
+function normalize(index, length) {
+  if (index > length) {
+    return length;
+  }
+
+  let normalizedIndex = (index < 0)
+    ? index + length
+    : index;
+
+  normalizedIndex = (normalizedIndex < 0)
+    ? 0
+    : normalizedIndex;
+
+  return normalizedIndex;
+}
+
 function applyCustomSplice() {
+  // SLICE method:
+  [].__proto__.slice2 = function(start = 0, end = this.length) {
+    const normalizedStart = normalize(start, this.length);
+    const normalizedEnd = normalize(end, this.length);
+
+    const result = [];
+
+    for (let i = normalizedStart; i < normalizedEnd; i++) {
+      result[result.length] = this[i];
+    }
+
+    return result;
+  };
+
   // CONCAT method:
   [].__proto__.concat2 = function(array) {
     return [...this, ...array];
@@ -48,15 +83,15 @@ function applyCustomSplice() {
     // |-- basically its an array of deleted values
     // |-- from the native array.
     // |-- And we"re splicing it before the original array mutated.
-    const spliced = this.slice(start, start + deleteCount);
+    const spliced = this.slice2(start, start + deleteCount);
 
     // - Here is the main <=podkapotniy=> BlAcK MaGiC of mutation
     // |-- in the native array to delete some values
     // |-- and add a new if needed ;)
     const mutation = this
-      .slice(0, start)
+      .slice2(0, start)
       .concat2(newItems)
-      .concat2(this.slice(start + deleteCount));
+      .concat2(this.slice2(start + deleteCount));
 
     // - Let"s change the length of or original array
     // |-- to add a new items.
