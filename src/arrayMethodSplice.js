b45e53d9d3f4f07c966135a6d50204a79730c4d0
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-15 19:16:47 +0300

    splice solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -5,7 +5,117 @@
  */
 function applyCustomSplice() {
   [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+    let thisLength = this.length;
+    const itemsLength = items.length;
+    const newArray = [];
+    const endArray = [];
+    let spliceStart = start;
+
+    if (start < 0) {
+      Math.abs(start) > thisLength
+        ? spliceStart = 0
+        : spliceStart = thisLength + spliceStart;
+    }
+
+    if (!start && !deleteCount && items.length === 0) {
+      return newArray;
+    }
+
+    if (spliceStart > thisLength) {
+      const thisNewLength = thisLength + itemsLength;
+
+      for (let i = thisLength, j = 0; i < thisNewLength; i++, j++) {
+        this[i] = items[j];
+      }
+
+      return newArray;
+    }
+
+    if (spliceStart >= 0 || (spliceStart === undefined && deleteCount > 0)) {
+      if (deleteCount === undefined) {
+        for (let i = spliceStart, j = 0; i < thisLength; i++, j++) {
+          newArray[j] = this[i];
+        }
+        this.length = spliceStart;
+
+        return newArray;
+      }
+
+      if (deleteCount <= 0) {
+        for (let i = spliceStart, j = 0; i < thisLength; i++, j++) {
+          newArray[j] = this[i];
+        }
+
+        for (let i = 0, j = spliceStart; i < itemsLength; i++, j++) {
+          this[j] = items[i];
+
+          if (i === itemsLength - 1) {
+            thisLength = j + 1;
+          }
+        }
+
+        if (itemsLength === 0) {
+          this.length = spliceStart;
+          thisLength = this.length;
+        }
+
+        for (let i = 0, j = thisLength; i < newArray.length; i++, j++) {
+          this[j] = newArray[i];
+        }
+        newArray.length = 0;
+
+        return newArray;
+      }
+
+      if (deleteCount > 0) {
+        if (spliceStart === undefined) {
+          spliceStart = 0;
+        }
+
+        let endOfSplice;
+
+        if ((deleteCount + spliceStart) < thisLength) {
+          endOfSplice = deleteCount + spliceStart;
+        } else {
+          endOfSplice = thisLength;
+        }
+
+        for (let i = spliceStart, j = 0, k = 0; i < thisLength; i++, j++) {
+          if (i < endOfSplice) {
+            newArray[j] = this[i];
+          } else {
+            endArray[k] = this[i];
+          }
+        }
+
+        for (let i = 0, j = spliceStart; i < itemsLength; i++, j++) {
+          this[j] = items[i];
+
+          if (i === itemsLength - 1) {
+            thisLength = j + 1;
+          }
+        }
+
+        if (itemsLength === 0) {
+          this.length = spliceStart;
+          thisLength = this.length;
+        }
+
+        for (let i = 0, j = thisLength; i < endArray.length; i++, j++) {
+          this[j] = endArray[i];
+
+          if (i === endArray.length - 1) {
+            this.length = j + 1;
+          }
+        }
+
+        if (newArray.length > deleteCount) {
+          newArray.length = deleteCount;
+        }
+
+        return newArray;
+      }
+    }
   };
 }
 
