e6ce39d9283954844ebef2bb38383a37a1d18075
Anna Borodaienko <anna.borodayenko@gmail.com>
2022-09-15 21:52:39 +0300

    Solution

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -3,9 +3,63 @@
 /**
  * Implement method Splice
  */
+
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(start = 0, deleteCount, ...items) {
+    if (start === 0 && !deleteCount && items.length === 0) {
+      return [];
+    }
+
+    const deletedItem = [];
+    const addedItems = [...items];
+    let startIndex = start;
+
+    if (start > this.length) {
+      startIndex = this.length;
+    }
+
+    if (start < 0) {
+      startIndex = this.length + start;
+    }
+
+    if (startIndex < 0) {
+      startIndex = 0;
+    }
+
+    const startIndexAdd = startIndex;
+
+    let deletedAmount = deleteCount;
+
+    if (!deleteCount && addedItems.length === 0) {
+      deletedAmount = this.length - startIndex;
+    }
+
+    if (deleteCount > (this.length - start)) {
+      deletedAmount = this.length - start;
+    }
+
+    if (deleteCount < 0) {
+      deletedAmount = 0;
+    }
+
+    for (let i = 0; i < deletedAmount; i++) {
+      deletedItem.push(this[startIndex]);
+      startIndex++;
+    }
+
+    const elementsAfterDeleted = this.slice(startIndexAdd + deletedAmount);
+
+    this.length = startIndexAdd;
+
+    for (let i = 0; i < addedItems.length; i++) {
+      this[startIndexAdd + i] = addedItems[i];
+    }
+
+    for (let i = 0; i < elementsAfterDeleted.length; i++) {
+      this[this.length] = elementsAfterDeleted[i];
+    }
+
+    return deletedItem;
   };
 }
 
