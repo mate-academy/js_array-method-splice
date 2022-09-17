568d4fc8ccb2036bb2ed113340b3226a10516140
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-09-17 17:50:12 +0300

    feat: custom splice method was created

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,52 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(begin, deleteCount, ...items) {
+    const deleted = [];
+    const len = this.length;
+    let start = begin > len ? len : begin;
+    let delCount = deleteCount;
+
+    start = start < 0 ? len + start : start;
+    start = start < 0 || start === undefined ? 0 : start;
+    start = start === null ? 0 : start;
+
+    if ((len === 0 && items.length === 0)
+      || delCount < 0
+      || (begin === undefined && delCount === undefined)
+    ) {
+      return deleted;
+    }
+
+    if (delCount + start > len || delCount === undefined) {
+      delCount = len - start;
+    }
+
+    for (let i = start; i < start + delCount; i++) {
+      deleted.push(this[i]);
+    }
+
+    for (let i = start + delCount, j = 0; i < len; i++, j++) {
+      this[start + j] = this[i];
+    }
+
+    this.length = len - delCount;
+
+    if (items.length && this.length) {
+      for (let i = start, j = 0; j < items.length; i++, j++) {
+        for (let n = this.length; n > start; n--) {
+          this[n] = this[n - 1];
+        }
+        start++;
+        this[i] = items[j];
+      }
+    } else if (items.length && this.length === 0) {
+      for (let i = 0; i < items.length; i++) {
+        this[i] = items[i];
+      }
+    }
+
+    return deleted;
   };
 }
 
