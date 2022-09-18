a8a438f961accdebcf3d8d7f354bf2b60078c5ea
Dima <ddikey1@gmail.com>
2022-09-18 12:14:15 +0300

    Solution for method splice

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,62 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(start = 0, deleteCount, ...items) {
+    const arrayBeforeSplice = [];
+    const arrayAfterSplice = [];
+    const deletedItems = [];
+
+    let startIndex = start;
+    let deleteElementNumber = deleteCount;
+
+    if (arguments.length === 0) {
+      return deletedItems;
+    }
+
+    if (startIndex > this.length) {
+      startIndex = this.length;
+    }
+
+    if (startIndex < 0) {
+      startIndex += this.length;
+    }
+
+    if (startIndex < 0) {
+      startIndex = 0;
+    }
+
+    if (deleteElementNumber === undefined
+    || deleteElementNumber > this.length - startIndex) {
+      deleteElementNumber = this.length - startIndex;
+    }
+
+    for (let i = 0; i < startIndex; i++) {
+      arrayBeforeSplice.push(this[i]);
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      if (i < startIndex + deleteElementNumber) {
+        deletedItems.push(this[i]);
+      } else {
+        arrayAfterSplice.push(this[i]);
+      }
+    }
+
+    let resultArray = [...arrayBeforeSplice, ...items, ...arrayAfterSplice];
+
+    if (items === undefined) {
+      resultArray = [...arrayBeforeSplice, ...arrayAfterSplice];
+    }
+
+    if (resultArray.length < this.length) {
+      this.length = 0;
+    }
+
+    for (let i = 0; i < resultArray.length; i++) {
+      this[i] = resultArray[i];
+    }
+
+    return deletedItems;
   };
 }
 
