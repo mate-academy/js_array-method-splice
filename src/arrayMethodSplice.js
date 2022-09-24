72435af7b88180e1df595eb527d5b7f0223d850c
Dima Romanenko <demigod177712@gmail.com>
2022-09-24 13:43:03 +0300

    total refactored code

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -3,131 +3,69 @@
 /**
  * Implement method Splice
  */
-function setStartingPoint(start, array) {
-  if (start > array.length) {
-    return array.length;
-  }
-
-  if (start === undefined || start < (array.length * -1)) {
-    return 0;
-  }
-
-  if (start < 0) {
-    return start + array.length;
-  }
-
-  return start;
-}
-
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
+  [].__proto__.splice2 = function(start = 0, deleteCount, ...items) {
+    if (start === 0 && !deleteCount && items.length === 0) {
+      return [];
+    }
+
+    let startIndex = start;
+    const addItems = [...items];
     const removedValues = [];
-    const startIndex = setStartingPoint(start, this);
-    const howManyDelete = deleteCount;
-    const addItems = items;
 
-    // case if splice without arguments or deleteCount < 0
-    if (arguments.length === 0 || howManyDelete < 0) {
-      return removedValues;
+    // normalizing start index
+    if (start > this.length) {
+      startIndex = this.length;
     }
 
-    // case if first two arguments === undefined
-    if (start === undefined && deleteCount === undefined && !addItems.length) {
-      return removedValues;
+    if (start < 0) {
+      startIndex = this.length + start;
     }
 
-    // case if all three arguments === undefined
-    if (start === undefined
-      && deleteCount === undefined
-      && addItems[0] === undefined) {
-      const reserved = [];
-
-      for (let i = 0; i < this.length; i++) {
-        reserved[i] = this[i];
-      }
-
-      this[0] = addItems[0];
-      this.length++;
-
-      for (let i = 1; i < this.length; i++) {
-        this[i] = reserved[i - 1];
-      }
-
-      return removedValues;
+    if (startIndex < 0) {
+      startIndex = 0;
     }
 
-    // case, when func receiving start index and adding elements
-    if (howManyDelete <= 0 && addItems.length !== 0) {
-      for (let i = this.length - 1; i >= startIndex; i--) {
-        this[i + addItems.length] = this[i];
-      }
+    const startIndexAdd = startIndex;
 
-      for (let i = 0; i < addItems.length; i++) {
-        this[i + startIndex] = addItems[i];
-      }
+    // normalizing delete
+    let howManyDelete = deleteCount;
 
-      return removedValues;
+    if (!deleteCount && addItems.length === 0) {
+      howManyDelete = this.length - startIndex;
     }
 
-    // case if only start point received
-    if (howManyDelete === undefined && addItems.length === 0) {
-      let v = 0;
-
-      for (let i = startIndex; i < this.length; i++) {
-        removedValues[v] = this[i];
-        v++;
-      }
-      this.length -= removedValues.length;
-
-      return removedValues;
+    if (deleteCount > (this.length - startIndex)) {
+      howManyDelete = this.length - startIndex;
     }
 
-    return replaceElems(
-      removedValues, startIndex, howManyDelete, addItems, this
-    );
+    if (howManyDelete + startIndex > this.length) {
+      howManyDelete = this.length - startIndex;
+    }
+
+    if (deleteCount < 0) {
+      howManyDelete = 0;
+    }
+
+    for (let i = 0; i < howManyDelete; i++) {
+      removedValues.push(this[startIndex]);
+      startIndex++;
+    }
+
+    const elementsAfterDeleted = this.slice(startIndexAdd + howManyDelete);
+
+    this.length = startIndexAdd;
+
+    for (let i = 0; i < addItems.length; i++) {
+      this[startIndexAdd + i] = addItems[i];
+    }
+
+    for (let i = 0; i < elementsAfterDeleted.length; i++) {
+      this[this.length] = elementsAfterDeleted[i];
+    }
+
+    return removedValues;
   };
-}
-
-function replaceElems(deleted, startIndex, howManyDelete, addItems, array) {
-  for (let i = 0; i < howManyDelete; i++) {
-    deleted[i] = array[startIndex + i];
-  }
-
-  const afterArr = [];
-  let v = 0;
-
-  for (let i = startIndex + howManyDelete; i < array.length; i++) {
-    afterArr[v] = array[i];
-    v++;
-  }
-
-  if (addItems.length === 0) {
-    for (let i = startIndex; i < array.length; i++) {
-      array[i] = afterArr[i - startIndex];
-    }
-
-    array.length -= deleted.length;
-
-    return deleted;
-  }
-
-  if (addItems.length > howManyDelete) {
-    array.length += addItems.length - howManyDelete;
-  }
-
-  if (addItems.length < howManyDelete) {
-    array.length -= howManyDelete - addItems.length;
-  }
-
-  for (let i = startIndex; i < startIndex + addItems.length; i++) {
-    array[i] = addItems[i - startIndex];
-  }
-
-  for (let i = startIndex + addItems.length; i < array.length; i++) {
-    array[i] = afterArr[i - (startIndex + addItems.length)];
-  }
-
-  return deleted;
 }
 
 module.exports = applyCustomSplice;
