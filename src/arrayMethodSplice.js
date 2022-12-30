d9440c9dca198696551d5813bbc193e17f50c273
Anna Inozemets <inoznetta@gmail.com>
2022-12-30 16:17:43 +0200

    Custom spliced method was realised

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,8 +4,50 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(start, deleteCount, ...items) {
-    // write code here
+  [].__proto__.splice2 = function(
+    start = 0,
+    deleteCount = this.length,
+    ...items
+  ) {
+    if (arguments.length === 0) {
+      return [];
+    }
+
+    if (deleteCount < 0) {
+      return [];
+    }
+
+    const splisedEl = [];
+    const elAfterSpliced = [];
+    let startIndex = start;
+    let howManyToDelete = deleteCount;
+
+    if (startIndex < 0) {
+      startIndex += this.length;
+    }
+
+    if (startIndex < 0) {
+      startIndex = 0;
+    }
+
+    if (startIndex > this.length) {
+      startIndex = 0;
+      howManyToDelete = 0;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i >= startIndex && i < startIndex + howManyToDelete) {
+        splisedEl.push(this[i]);
+      } else if (i >= startIndex + howManyToDelete) {
+        elAfterSpliced.push(this[i]);
+      }
+    }
+
+    this.length = startIndex;
+
+    this.push(...items, ...elAfterSpliced);
+
+    return splisedEl;
   };
 }
 
