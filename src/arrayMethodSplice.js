1c927f9d9fb61dd72997093280b8ba0fef8c957f
Anna Inozemets <inoznetta@gmail.com>
2022-12-30 22:23:02 +0200

    Solution was update

--- src/arrayMethodSplice.js
+++ src/arrayMethodSplice.js
@@ -4,54 +4,87 @@
  * Implement method Splice
  */
 function applyCustomSplice() {
-  [].__proto__.splice2 = function(
-    start = 0,
-    deleteCount = this.length,
-    ...items
-  ) {
-    if (arguments.length === 0) {
-      return [];
+  [].__proto__.push2 = function(...forPushed) {
+    for (let i = 0; i < forPushed.length; i++) {
+      this[this.length] = forPushed[i];
     }
 
-    if (deleteCount < 0) {
-      return [];
-    }
+    return this;
+  };
 
+  [].__proto__.splice2 = function(start, deleteCount, ...items) {
+    const args = [...arguments];
     const splisedEl = [];
     const elAfterSpliced = [];
+
     let startIndex = start;
-    let howManyToDelete = deleteCount;
+    let toDelete = deleteCount;
 
-    if (startIndex < 0) {
-      startIndex += this.length;
+    if (args.length === 0) {
+      return splisedEl;
     }
 
-    if (startIndex < 0) {
-      startIndex = 0;
+    if (args.length === 1) {
+      if (startIndex === undefined) {
+        startIndex = 0;
+      }
+
+      if (startIndex < 0) {
+        startIndex += this.length;
+      }
+
+      if (startIndex < 0) {
+        startIndex = 0;
+      }
+
+      toDelete = this.length;
+
+      if (startIndex > this.length) {
+        toDelete = 0;
+        startIndex = 0;
+      }
     }
 
-    if (startIndex > this.length) {
-      startIndex = 0;
-      howManyToDelete = 0;
+    if (args.length > 1) {
+      if (startIndex === undefined && toDelete === undefined) {
+        return splisedEl;
+      }
+
+      if (startIndex === undefined) {
+        startIndex = 0;
+      }
+
+      if (startIndex < 0) {
+        startIndex += this.length;
+      }
+
+      if (startIndex < 0) {
+        startIndex = 0;
+      }
+
+      if (startIndex > this.length) {
+        toDelete = 0;
+        startIndex = 0;
+      }
+
+      if (toDelete === undefined) {
+        toDelete = this.length;
+      }
+
+      if (toDelete < 0) {
+        return splisedEl;
+      }
     }
 
     for (let i = 0; i < this.length; i++) {
-      if (i >= startIndex && i < startIndex + howManyToDelete) {
+      if (i >= startIndex && i < startIndex + toDelete) {
         splisedEl.push(this[i]);
-      } else if (i >= startIndex + howManyToDelete) {
+      } else if (i >= startIndex + toDelete) {
         elAfterSpliced.push(this[i]);
       }
     }
 
     this.length = startIndex;
-
-    [].__proto__.push2 = function(...forPushed) {
-      for (let i = 0; i < forPushed.length; i++) {
-        this[this.length] = forPushed[i];
-      }
-
-      return this;
-    };
 
     this.push2(...items, ...elAfterSpliced);
 
